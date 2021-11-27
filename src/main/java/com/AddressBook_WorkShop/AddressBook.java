d6002cf41b86ed4e6cfa3edadd24b9939dd97c44
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-27 21:05:28 +0530

    Updated: Files With UC5

--- src/main/java/com/AddressBook_WorkShop/AddressBook.java
+++ src/main/java/com/AddressBook_WorkShop/AddressBook.java
@@ -99,10 +99,74 @@ public class AddressBook
         }
     }
 	
+	public void display() 
+	{
+		System.out.println(" ");
+		System.out.println("First Name:: " + first_Name);
+		System.out.println("Last Name:: " + last_Name);
+		System.out.println("Email:: " + email_Id);
+		System.out.println("Address:: " + address);
+		System.out.println("City Name:: " + city);
+		System.out.println("State Name:: " + state);
+		System.out.println("Zip Code:: " + zip_Code);
+		System.out.println("Phone Number:: " + phone_Number);
+	}
+
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
 		AddressBook ab = new AddressBook();
-		ab.addContactDetails();
-		ab.editContactUsingFirstName();
+		System.out.println("0. Exit");
+		System.out.println("1. Add Contact");
+		System.out.println("2. Edit Contact");
+		System.out.println("3. Delete Contact");
+		System.out.println("4. Display All Contacts");
+		System.out.println("Enter Choice:: ");
+		int choice = sc.nextInt();
+		while(choice != 0)
+		{
+			switch(choice)
+			{
+			case 1:
+				ab.addContactDetails();
+				break;
+				
+			case 2:
+				ab.editContactUsingFirstName();
+				break;
+				
+			case 3:
+				ab.deleteContactDetails();
+				break;
+				
+			case 4:
+				for(Contacts contact: contacts)
+				{
+					System.out.println(" ");
+					System.out.println("First Name:: "+contact.getFirstName());
+					System.out.println("Last Name:: "+contact.getLastName());
+					System.out.println("Email:: "+contact.getEmailId());
+					System.out.println("Address:: "+contact.getAddress());
+					System.out.println("City Name:: "+contact.getCity());
+					System.out.println("State Name:: "+contact.getState());
+					System.out.println("Zip Code:: "+contact.getZipCode());
+					System.out.println("Phone Number:: "+contact.getPhoneNumber());
+				}
+				break;
+				
+			default :
+				System.out.println("Wront Input");
+				break;
+				
+			}
+			System.out.println("0. Exit");
+			System.out.println("1. Add Contact");
+			System.out.println("2. Edit Contact");
+			System.out.println("3. Delete Contact");
+			System.out.println("4. Display All Contacts");
+			System.out.println("Enter Choice:: ");
+			choice = sc.nextInt();
+		}	
+		System.out.println("Program Got Exit");
 	}
 }
